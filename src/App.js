f8368b78b1c2d40015e17a832d02bb438a5381c4
Manuel Gomez <mpr.gomezc@gmail.com>
2021-04-23 09:14:37 +0200

    Added routing for the site and basic layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { 
+  EuiPage,
+  EuiPageBody
+} from '@elastic/eui'
+import '@elastic/eui/dist/eui_theme_light.css'
+import React from 'react'
+import { Switch, Route } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
+import Header from './components/header/header';
+import Home from './pages/home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{minHeight:"100vh", display:"flex"}}>
+      <BrowserRouter>
+      <Header />
+      <EuiPage restrictWidth style={{paddingTop:"50px"}}>
+        <EuiPageBody>
+          <Switch>
+            <Route path="/" exact component={Home} />
+          </Switch>
+        </EuiPageBody>
+      </EuiPage>
+      </BrowserRouter>
     </div>
   );
 }
